959233574a3e00f6ddda709e91cf73ddf59b47c1
Igor Silva <igor.silva@neppo.com.br>
2024-02-01 16:35:38 -0300

    Initial commit

---
+++ travel-guide/travel-guide/src/screens/Home.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export function Home() {
+  return (
+    <View className="flex-1">
+        <Text className="">Hello</Text>
+    </View>
+    
+  )
+}
